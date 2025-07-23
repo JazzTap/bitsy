e65c249ff6119f3c8cf72c3840e4a70f04467950
⚗ Jasmine Otto <ottojasmine@gmail.com>
2025-07-23 08:22:27 -0700

    update port forwarding address

--- editor/script/system/multiplayer.js
+++ editor/script/system/multiplayer.js
@@ -16,7 +16,7 @@ export async function attachServer(debug = false) {
     // Then set up an automerge repo (loading with our annoying WASM hack)
     const repo = new AutomergeRepo.Repo({
         storage: new IndexedDBStorageAdapter(),
-        network: [new BrowserWebSocketClientAdapter("wss://ab6022e48f84.ngrok-free.app")],
+        network: [new BrowserWebSocketClientAdapter("wss://4f46fceb5ea9.ngrok-free.app")],
     })
     let handle
 
